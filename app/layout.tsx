1e2d6422107f448ba0f0a843fcea863b28c299b2
MD MONIRUL ISLAM <monirul.dev@gmail.com>
2024-08-27 01:51:11 +0600

    Add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,48 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import React from "react";
+
+export const metadat: Metadata = {
+  title: "Next.js 14 With Clerk",
+  description: "A community-driven platform",
+  icons: {
+    icon: "assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-space-grotesk'
+})
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+     appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover:text-primary-500'
+      }
+     }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
-        </body>
+          </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
