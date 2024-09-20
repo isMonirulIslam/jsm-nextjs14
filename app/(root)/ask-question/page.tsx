3912f4a132c3691e489a08aecf303e2d615e3673
MD MONIRUL ISLAM <monirul.dev@gmail.com>
2024-09-20 18:32:07 +0600

    Remove MongoDB User Id With Auth()

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,5 @@
 import Question from "@/components/forms/Question";
-import { getUserById } from "@/lib/actions/user.action";
+// import { getUserById } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -7,13 +7,13 @@ import React from "react";
 const AskQuestion = async () => {
   const { userId } = auth();
   if (!userId) redirect("/sign-in");
-  const mongoUser = await getUserById({ userId });
+  // const mongoUser = await getUserById({ userId });
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+        <Question mongoUserId={JSON.stringify(userId)} />
       </div>
     </div>
   );
